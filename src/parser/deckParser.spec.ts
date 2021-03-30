14c295a0f3b533cd256ff5a52df00459760f0988
Your Lord and Master <kuriosly@gmail.com>
2021-03-30 13:40:27 -0700

    tests, library setup, etc...

--- src/parser/deckParser.spec.ts
+++ src/parser/deckParser.spec.ts
@@ -1,13 +1,15 @@
-import { Parser } from './parser';
+import { DeckParser } from './deckParser';
 import { expect } from 'chai';
 
 describe('Deck Paser',()=>{
     it('Test Parse 97ya Deck Code',()=>{
         const deck97ya = "DCTrzRBiEJiBUoACi14ACD8oACpj5jSqEoACqh4rTAn4ACIngACgn4ACgjbcKJtQgqoRpxyhyAAChroAELD4AEAbZxyIJYrSgR6OCEnIACIbRxyIZBjSJj5jSA/grSkNYACCxIACIRpx0DDoACA5orSIbIrSQfgACCqwACCrgACobZxzAbZxwA=="
-        const data = Parser.parse(deck97ya);
-        expect(data).to.exist;
-        expect(data.division).to.equal(431);
-        expect(data.cardCount).to.equal(34);
-        expect(data.income).to.equal(1);
+        const data = DeckParser.parse(deck97ya);
+        expect(data.raw).to.exist;
+        expect(data.raw.division).to.equal(431);
+        expect(data.raw.cardCount).to.equal(34);
+        expect(data.raw.income).to.equal(1);
+
+        console.log(JSON.stringify(data))
     })
 })
---
+++ src/parser/deckParser.ts
@@ -0,0 +1,115 @@
+
+import { units, divisions, misc } from 'sd2-data';
+export class DeckParser{
+
+  static parseRaw(code:string):DeckDataRaw{
+      const base64data = Buffer.from(code, "base64");
+      let binaryData = "";
+
+      base64data.forEach((x)=>{
+        binaryData = binaryData.concat(x.toString(2).padStart(8,"0"));
+      })
+      return DeckParser.getHeader(binaryData);
+  }
+  static makePretty(deck:DeckDataRaw):DeckData{
+    const income = misc.incomeTypes[deck.income] || "ERROR"
+    const div = divisions.divisionsById[deck.division].name || "ERROR";
+    const ret:DeckData = {
+      raw: deck,
+      income: income,
+      division: div,
+      units: []
+    }
+    for(const unit of deck.units){
+      //console.log(JSON.stringify(units.unitsById))
+      const u = units.unitsById[unit.id]?.name || "ERROR"
+      let t = "None"
+      if(unit.transportid != -1)
+        t = units.unitsById[unit.transportid]?.name || "ERROR"
+      const punit:Unit = {
+        xp:unit.xp,
+        phase:unit.phase,
+        count:unit.count,
+        name:u,
+        transport:t,
+        raw:unit
+      };
+      ret.units.push(punit)
+    }
+    return ret;
+  }
+
+  static parse(code:string):DeckData{
+    return DeckParser.makePretty(DeckParser.parseRaw(code));
+  }
+
+  private static getHeader(binData:string):DeckDataRaw{
+
+      const pop = (count:number) => {
+           const ret = parseInt(binData.slice(0, count), 2);
+          binData = binData.slice(count);
+        return ret;
+      }
+      const header:number[] = [];
+      for(let i=0;i<5;i++){
+        const count = pop(5);
+        header.push(pop(count));
+      } 
+  
+      const countLen = pop(5);
+      const phaseLen = pop(5);
+      const xpLen = pop(5);
+      const unitLen = pop(5);
+      const cardsCount = header[3];
+      const units: UnitRaw[] = [];
+  
+      for(let i=0;i<cardsCount;i++){
+        const count = pop(countLen);
+        const phase = pop(phaseLen);
+        const xp = pop(xpLen);
+        const id = pop(unitLen)-1;
+        const transportid = pop(unitLen)-1;
+        
+
+        const unit:UnitRaw = {count: count, phase: phase, xp: xp, id: id, transportid: transportid}
+        units.push(unit);
+      }
+
+      const result:DeckDataRaw = {division: header[2], cardCount: header[3], income: header[4], units: units};
+  
+      return result;
+  }
+
+  
+}
+
+declare type DeckDataRaw = {
+income: number;
+division: number;
+cardCount: number;
+units: UnitRaw[];
+}
+
+declare type UnitRaw = {
+id: number;
+transportid: number;
+xp: number;
+phase: number;
+count: number;
+}
+
+declare type DeckData = {
+  income: string;
+  division: string;
+  units: Unit[];
+  raw: DeckDataRaw;
+}
+
+declare type Unit = {
+  xp:number;
+  phase:number;
+  count: number;
+  name: string;
+  transport: string;
+  raw:UnitRaw;
+}
