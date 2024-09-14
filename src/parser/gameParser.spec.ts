c63b5d5e5a9208df412033f932511d2a114a0734
ReeF <vesecky.lukas90@gmail.com>
2024-09-14 13:19:01 +0200

    moved data extracting methods from sodbot, stopped using misc.maps

--- src/parser/gameParser.spec.ts
+++ src/parser/gameParser.spec.ts
@@ -1,160 +1,195 @@
-import { GameParser, RawGameData } from './gameParser';
+import {GameParser, RawGameData} from './gameParser';
 import * as fs from 'fs'
-import { expect } from 'chai';
+import {expect} from 'chai';
 
 describe('Game Parser', () => {
-  it('Test parse old replay', () => {
-    const buffer = fs.readFileSync("test/hellaOldReplay.rpl3");
-    let data = GameParser.parseRaw(buffer);
+    it('Test parse oldest replay', () => {
+        const buffer = fs.readFileSync("test/hellaOldReplay.rpl3");
+        let data = GameParser.parseRaw(buffer);
 
-    data = data as RawGameData;
-    expect(data).not.null;
-  }),
-    it('Test load players ReeF vs Iwon', () => {
-      const buffer = fs.readFileSync("test/Reef_vs_Iwon.rpl3");
-      let data = GameParser.parseRaw(buffer);
-
-      expect(data).not.null;
-
-      data = data as RawGameData;
-
-      expect(data.players[0].name).is.equal('[Duke] IwonTheInternet');
-      expect(data.players[1].name).is.equal('ReeF');
-
+        data = data as RawGameData;
+        expect(data).not.null;
     }),
-    it('Test load divs ReeF vs Iwon', () => {
-      const buffer = fs.readFileSync("test/Reef_vs_Iwon.rpl3");
-      let data = GameParser.parseRaw(buffer);
+        it('Test parse old replay', () => {
+            const buffer = fs.readFileSync("test/g0af_vs_Metalphase_G2.rpl3");
+            let data = GameParser.parseRaw(buffer);
 
-      expect(data).not.null;
+            data = data as RawGameData;
+            expect(data).not.null;
+        }),
+        it('Test load players ReeF vs Iwon', () => {
+            const buffer = fs.readFileSync("test/Reef_vs_Iwon.rpl3");
+            let data = GameParser.parseRaw(buffer);
 
-      data = data as RawGameData;
+            expect(data).not.null;
 
-      expect(data.players[0].deck?.division).is.equal('1. Lovas');
-      expect(data.players[1].deck?.division).is.equal('Panssaridivisioona');
-    }),
-    it('Test load players Skumfuk vs Hard AI', () => {
-      const buffer = fs.readFileSync("test/Conquest_Break_1v1.rpl3");
-      const data = GameParser.parseRaw(buffer);
+            data = data as RawGameData;
 
-      expect(data!.players[0].name).is.equal('Skumfuk')
-      expect(data!.players[1].name).is.equal('AI Hard')
+            expect(data.players[0].name).is.equal('[Duke] IwonTheInternet');
+            expect(data.players[1].name).is.equal('ReeF');
 
-    }),
-    it('Test load factions + divs Skumfuk vs Hard AI', () => {
-      const buffer = fs.readFileSync("test/Conquest_Break_1v1.rpl3");
-      const data = GameParser.parseRaw(buffer);
+        }),
+        it('Test load divs ReeF vs Iwon', () => {
+            const buffer = fs.readFileSync("test/Reef_vs_Iwon.rpl3");
+            let data = GameParser.parseRaw(buffer);
 
-      expect(data!.players[0].deck?.division).is.equal('14. Infanterie');
-      expect(data!.players[1].deck?.division).is.equal('15th Infantry');
+            expect(data).not.null;
 
-      expect(data!.players[0].deck?.faction).is.not.true;
-      expect(data!.players[1].deck?.faction).is.true;
-    }),
+            data = data as RawGameData;
+
+            expect(data.players[0].deck?.division).is.equal('1. Lovas');
+            expect(data.players[1].deck?.division).is.equal('Panssaridivisioona');
+        }),
+        it('Test load players Skumfuk vs Hard AI', () => {
+            const buffer = fs.readFileSync("test/Conquest_Break_1v1.rpl3");
+            const data = GameParser.parseRaw(buffer);
+
+            expect(data!.players[0].name).is.equal('Skumfuk')
+            expect(data!.players[1].name).is.equal('AI Hard')
+
+        }),
+        it('Test load factions + divs Skumfuk vs Hard AI', () => {
+            const buffer = fs.readFileSync("test/Conquest_Break_1v1.rpl3");
+            const data = GameParser.parseRaw(buffer);
+
+            expect(data!.players[0].deck?.division).is.equal('14. Infanterie');
+            expect(data!.players[1].deck?.division).is.equal('15th Infantry');
+
+            expect(data!.players[0].deck?.faction).is.not.true;
+            expect(data!.players[1].deck?.faction).is.true;
+        }),
+        it('Test stability when incorrect input', () => {
+            const buffer = fs.readFileSync("test/emptyFile.rpl3");
+            const data = GameParser.parseRaw(buffer);
+
+            expect(data).is.null;
+        }),
+        it('Test load AS vs VonPaulus_G2', () => {
+            const buffer = fs.readFileSync("test/AS_vs._VonPaulus_G2.rpl3");
+            const data = GameParser.parseRaw(buffer);
+
+            //json object
+            const testObj = {
+                game: {
+                    GameMode: '1',
+                    IsNetworkMode: '1',
+                    NbMaxPlayer: '2',
+                    NbPlayersAndIA: '2',
+                    AllowObservers: '1',
+                    ObserverDelay: '120',
+                    Seed: '233158952',
+                    Private: '1',
+                    ServerName: "Definitely not ÁS's Game",
+                    WithHost: '1',
+                    ServerProtocol: '1.0',
+                    TimeLeft: '4294967295',
+                    Version: '47028',
+                    GameState: '0',
+                    NeedPassword: '0',
+                    NbIA: '0',
+                    TickRate: '10',
+                    UniqueSessionId: '8e3bf97:a5498aae:f3b0599e:19d5f85d',
+                    ModList: '',
+                    ModTagList: '',
+                    ServerTag: '',
+                    GameType: '2',
+                    Map: '_2x2_Lacs_Sianno_LD_1v1',
+                    InitMoney: '750',
+                    TimeLimit: '0',
+                    ScoreLimit: '2000',
+                    VictoryCond: '2',
+                    CombatRule: '2',
+                    IncomeRate: '3',
+                    WarmupCountdown: '10',
+                    DeploiementTimeMax: '180',
+                    DebriefingTimeMax: '180',
+                    LoadingTimeMax: '1200',
+                    NbMinPlayer: '10',
+                    DeltaMaxTeamSize: '10',
+                    MaxTeamSize: '10',
+                    PhaseADuration: '-1',
+                    PhaseBDuration: '-1',
+                    MapSelection: '0',
+                    MapRotationType: '0',
+                    CoopVsAI: '0',
+                    InverseSpawnPoints: '0',
+                    DivisionTagFilter: 'DEFAULT',
+                    AutoFillAI: '0',
+                    DeltaTimeCheckAutoFillAI: '60'
+                },
+                player_1: {
+                    PlayerUserId: '1434658',
+                    PlayerIALevel: '-1',
+                    PlayerObserver: '0',
+                    PlayerAlliance: '0',
+                    PlayerReady: '0',
+                    PlayerElo: '1486.47295235',
+                    PlayerLevel: '14',
+                    PlayerName: 'Definitely not ÁS',
+                    PlayerTeamName: '',
+                    PlayerAvatar: 'VirtualData/SteamGamerPicture/76561198116286575',
+                    PlayerDeckContent: 'DCTsjSBiEJihbAAEUYELaEewACMTEaCsc8OCMc8OCkeIACMSsaDEUsaCEeAACsdMaCEf4ACkdEaCEU0aCMdMaC0NsQSMLsQSkZ4ACENULaESkLaMRkQSERUaCg6wACkZEaCsSsaCENMZqkMcX6UQkQSUQsQSxH4ACUOcaCEhoACkgIACqgYADBH4ACUNsQQ=',
+                    PlayerSkinIndexUsed: 'DNQwJH8=',
+                    PlayerIsAIAutoFilled: '0',
+                    PlayerScoreLimit: '2000',
+                    PlayerIncomeRate: '1'
+                },
+                player_3: {
+                    PlayerUserId: '1048751',
+                    PlayerIALevel: '-1',
+                    PlayerObserver: '0',
+                    PlayerAlliance: '1',
+                    PlayerReady: '1',
+                    PlayerElo: '1854.6270814',
+                    PlayerLevel: '36',
+                    PlayerName: 'RTK {S} | PauluS',
+                    PlayerTeamName: '',
+                    PlayerAvatar: 'VirtualData/SteamGamerPicture/76561197971998618',
+                    PlayerDeckContent: 'DCTAzRhBEJkGhyDsNDkHRaMIOq0YQdlowg6jHgAEWkCDqtDEHQaKAnkLki5FoYg6jIoAEGcgAKM5AAQY7AAiWKABQuwACRDSLlCHiDsDqgAQNgAAwa0ABUzgACKvoAEBZgAINEIuUaIGEoQiABFS8AChBKUdVCSDoNEkHWaLLSg0SWzJosCc',
+                    PlayerSkinIndexUsed: 'DLVgQA==',
+                    PlayerIsAIAutoFilled: '0',
+                    PlayerScoreLimit: '2000',
+                    PlayerIncomeRate: '1'
+                },
+                ingamePlayerId: 0,
+                result: {Duration: '1133', Victory: '5', Score: '0'}
+            }
+
+            console.log('MAAAAAAAAAP:', data!.mapName);
+
+            expect(data!.result.duration).is.equal(Number(testObj.result.Duration))
+            expect(data!.result.score).is.equal(Number(testObj.result.Score))
+            expect(data!.result.victory).is.equal(Number(testObj.result.Victory))
+
+            expect(data!.scoreLimit).is.equal(Number(testObj.game.ScoreLimit))
+            expect(data!.timeLimit).is.equal(Number(testObj.game.TimeLimit))
+            expect(data!.incomeRate).is.equal(Number(testObj.game.IncomeRate))
+            expect(data!.gameMode).is.equal(Number(testObj.game.GameMode))
+            expect(data!.initMoney).is.equal(Number(testObj.game.InitMoney))
+            expect(data!.mapRaw).is.equal(testObj.game.Map)
+            expect(data!.mapName).is.equal('Sianno')
+
+            fs.writeFileSync("output.txt", JSON.stringify(data));
+        }),
+        it('Test map names', () => {
+            expect(GameParser.getMapName('_3x2_Tali_Ihantala_LD_1v1_CQC')).is.equal('Tali Ihantala');
+
+            expect(GameParser.getMapName('_3x2_Slutsk_LD_3v3_CQC')).is.equal('Slutsk 3v3');
+
+            expect(GameParser.getMapName('_3x3_MountRiver_1vs1')).is.equal('Mount River');
+
+            expect(GameParser.getMapName('_2x3_Vertigo_1vs1_CONQ_DUEL')).is.equal('Vertigo');
+
+            expect(GameParser.getMapName('_5x2_Loop_2vs2_CONQ_DUEL')).is.equal('Loop 2v2');
+
+            expect(GameParser.getMapName('_4x3_geisa_10vs10_DEST')).is.equal('Geisa 10v10');
+
+            expect(GameParser.getMapName('_2x3_zigzag_2vs2_DEST')).is.equal('Vertigo 2v2');
+
+            expect(GameParser.getMapName('_3x3_IFleuve')).is.equal('Mount River');
+
+            expect(GameParser.getMapName('_2x2_Plateau_Central_Orsha_E_LD_1v1')).is.equal('Orsha East');
 
 
-    it('Test load AS vs VonPaulus_G2', () => {
-      const buffer = fs.readFileSync("test/AS_vs._VonPaulus_G2.rpl3");
-      const data = GameParser.parseRaw(buffer);
-
-      //json object
-      const testObj = {
-        game: {
-          GameMode: '1',
-          IsNetworkMode: '1',
-          NbMaxPlayer: '2',
-          NbPlayersAndIA: '2',
-          AllowObservers: '1',
-          ObserverDelay: '120',
-          Seed: '233158952',
-          Private: '1',
-          ServerName: "Definitely not ÁS's Game",
-          WithHost: '1',
-          ServerProtocol: '1.0',
-          TimeLeft: '4294967295',
-          Version: '47028',
-          GameState: '0',
-          NeedPassword: '0',
-          NbIA: '0',
-          TickRate: '10',
-          UniqueSessionId: '8e3bf97:a5498aae:f3b0599e:19d5f85d',
-          ModList: '',
-          ModTagList: '',
-          ServerTag: '',
-          GameType: '2',
-          Map: '_2x2_Lacs_Sianno_LD_1v1',
-          InitMoney: '750',
-          TimeLimit: '0',
-          ScoreLimit: '2000',
-          VictoryCond: '2',
-          CombatRule: '2',
-          IncomeRate: '3',
-          WarmupCountdown: '10',
-          DeploiementTimeMax: '180',
-          DebriefingTimeMax: '180',
-          LoadingTimeMax: '1200',
-          NbMinPlayer: '10',
-          DeltaMaxTeamSize: '10',
-          MaxTeamSize: '10',
-          PhaseADuration: '-1',
-          PhaseBDuration: '-1',
-          MapSelection: '0',
-          MapRotationType: '0',
-          CoopVsAI: '0',
-          InverseSpawnPoints: '0',
-          DivisionTagFilter: 'DEFAULT',
-          AutoFillAI: '0',
-          DeltaTimeCheckAutoFillAI: '60'
-        },
-        player_1: {
-          PlayerUserId: '1434658',
-          PlayerIALevel: '-1',
-          PlayerObserver: '0',
-          PlayerAlliance: '0',
-          PlayerReady: '0',
-          PlayerElo: '1486.47295235',
-          PlayerLevel: '14',
-          PlayerName: 'Definitely not ÁS',
-          PlayerTeamName: '',
-          PlayerAvatar: 'VirtualData/SteamGamerPicture/76561198116286575',
-          PlayerDeckContent: 'DCTsjSBiEJihbAAEUYELaEewACMTEaCsc8OCMc8OCkeIACMSsaDEUsaCEeAACsdMaCEf4ACkdEaCEU0aCMdMaC0NsQSMLsQSkZ4ACENULaESkLaMRkQSERUaCg6wACkZEaCsSsaCENMZqkMcX6UQkQSUQsQSxH4ACUOcaCEhoACkgIACqgYADBH4ACUNsQQ=',
-          PlayerSkinIndexUsed: 'DNQwJH8=',
-          PlayerIsAIAutoFilled: '0',
-          PlayerScoreLimit: '2000',
-          PlayerIncomeRate: '1'
-        },
-        player_3: {
-          PlayerUserId: '1048751',
-          PlayerIALevel: '-1',
-          PlayerObserver: '0',
-          PlayerAlliance: '1',
-          PlayerReady: '1',
-          PlayerElo: '1854.6270814',
-          PlayerLevel: '36',
-          PlayerName: 'RTK {S} | PauluS',
-          PlayerTeamName: '',
-          PlayerAvatar: 'VirtualData/SteamGamerPicture/76561197971998618',
-          PlayerDeckContent: 'DCTAzRhBEJkGhyDsNDkHRaMIOq0YQdlowg6jHgAEWkCDqtDEHQaKAnkLki5FoYg6jIoAEGcgAKM5AAQY7AAiWKABQuwACRDSLlCHiDsDqgAQNgAAwa0ABUzgACKvoAEBZgAINEIuUaIGEoQiABFS8AChBKUdVCSDoNEkHWaLLSg0SWzJosCc',
-          PlayerSkinIndexUsed: 'DLVgQA==',
-          PlayerIsAIAutoFilled: '0',
-          PlayerScoreLimit: '2000',
-          PlayerIncomeRate: '1'
-        },
-        ingamePlayerId: 0,
-        result: { Duration: '1133', Victory: '5', Score: '0' }
-      }
-
-      expect(data!.result.duration).is.equal(Number(testObj.result.Duration))
-      expect(data!.result.score).is.equal(Number(testObj.result.Score))
-      expect(data!.result.victory).is.equal(Number(testObj.result.Victory))
-
-      expect(data!.scoreLimit).is.equal(Number(testObj.game.ScoreLimit))
-      expect(data!.timeLimit).is.equal(Number(testObj.game.TimeLimit))
-      expect(data!.incomeRate).is.equal(Number(testObj.game.IncomeRate))
-      expect(data!.gameMode).is.equal(Number(testObj.game.GameMode))
-      expect(data!.initMoney).is.equal(Number(testObj.game.InitMoney))
-      expect(data!.map_raw).is.equal(testObj.game.Map)
-
-      fs.writeFileSync("output.txt", JSON.stringify(data));
-    })
+        });
 })
--- src/parser/gameParser.ts
+++ src/parser/gameParser.ts
@@ -7,6 +7,7 @@ export class GameParser {
     static parseRaw(gameData: Buffer): RawGameData | null {
 
         try {
+
             //figure out junk length:
             //const junk = gameData.toString().split("{\"game\":")[0].length
             const junk = gameData.indexOf("{\"game\":")
@@ -16,7 +17,7 @@ export class GameParser {
             const d = gameData.slice((junk)).toString()
             let matches = regex.exec(d);
 
-            //old replays miss the ingamePlayerId
+            //some (old?) replays miss the ingamePlayerId
             if (!matches) {
                 regex = /("PlayerIncomeRate":"\d"}})/g
                 matches = regex.exec(d);
@@ -51,7 +52,8 @@ export class GameParser {
             ret.tickRate = Number(startData.game.TickRate)
             ret.uniqueSessionId = String(startData.game.UniqueSessionId)
             ret.gameType = Number(startData.game.GameType)
-            ret.map_raw = String(startData.game.Map)
+            ret.mapRaw = String(startData.game.Map)
+            ret.mapName = GameParser.getMapName(ret.mapRaw);
             ret.initMoney = Number(startData.game.InitMoney)
             ret.timeLimit = Number(startData.game.TimeLimit)
             ret.scoreLimit = Number(startData.game.ScoreLimit)
@@ -64,6 +66,8 @@ export class GameParser {
                 score: Number(startData.result.Score)
             }
 
+
+            const gameResult = ret.result.victory > 3 ? ret.ingamePlayerId >= ret.players.length / 2 : ret.ingamePlayerId < ret.players.length / 2;
 
             //build players and append them
             for (const key of Object.keys(startData)) {
@@ -89,44 +93,118 @@ export class GameParser {
                         p.deck = DeckParser.parse(pl.PlayerDeckContent)
                     p.scoreLimit = Number(pl.PlayerScoreLimit)
                     p.incomeRate = Number(pl.PlayerIncomeRate)
+
+
+                    p.winner = gameResult ? p.alliance === 1 : p.alliance === 0;
+
+
                     ret.players.push(p)
                 }
             }
+
+            ret.validForUpload = GameParser.isValidReplay(ret);
             return ret;
         } catch (e) {
             console.log(e)
             return null;
         }
     }
+    static getMapName(mapRaw: string): string {
+
+        let map = '';
+
+        if(!mapRaw.includes('_')){
+            return mapRaw;
+        }
+
+        const arr = mapRaw.split('_');
+
+        if(arr.length < 3){
+            return mapRaw;
+        }
+
+        map = arr[2];
+        let counter = 3;
+        while (counter < arr.length && isNaN(parseInt(arr[counter][0]))) {
+
+            if (arr[counter] === 'LD') {
+                break;
+            }
+
+            map += arr[counter];
+            counter++;
+        }
+
+            const exception = misc.mapExceptions[map];
+
+            map = exception ? exception : map;
+
+            map = map.replace(/([A-Z])/g, ' $1').trim();
+
+
+        const match = mapRaw.match(/([0-9])vs([0-9])/) ?? mapRaw.match(/([0-9])v([0-9])/);
+
+        if(match !== null && match[0] !== '1vs1' && match[0] !== '1v1'){
+
+
+            let mapType = match[0].replace('vs', 'v');
+
+            mapType = mapType === '0v1' ? '10v10' : mapType;
+
+            map += ' ' + mapType;
+
+            const firstChar = map[0].toUpperCase();
+
+            map = firstChar + map.slice(1);
+
+        }
+
+        return map;
+    }
+
+    static isValidReplay(g: RawGameData): string[] | null {
+        const isValid = [];
+
+        if (g.aiCount > 0) isValid.push("aiCount");
+        if (g.gameMode != 1) isValid.push("gameMode");
+        if (g.incomeRate != 3 && g.players.length == 2) isValid.push("incomeRate"); //don't accept other for 1v1s
+        if (g.scoreLimit != 2000) isValid.push("scoreLimit");
+        if (g.playerCount === 1) isValid.push("playerCount");
+
+        return isValid.length > 0 ? isValid : null;
+    }
+
 }
 
 //mod list/support is not supported atm.
 //Server tag not implemented
 export class RawGameData {
-    gameMode = -1
-    isNetworkMode = false
-    maxPlayers = 0
-    playerCount = 0
-    allowObservers = false
-    observerDelay = -1
-    seed = -1
-    isPrivate = true
-    serverName = ""
-    withHost = true
-    serverProtocol = ""
-    version = -1
-    aiCount = 0
-    tickRate = 10
-    uniqueSessionId = ''
-    gameType = -1
-    map_raw = ""
-    initMoney = 0
-    timeLimit = 0
-    scoreLimit = 0
-    victoryCondition = 0
-    incomeRate = 0
+    gameMode = -1;
+    isNetworkMode = false;
+    validForUpload: string[] | null = null;
+    maxPlayers = 0;
+    playerCount = 0;
+    allowObservers = false;
+    observerDelay = -1;
+    seed = -1;
+    isPrivate = true;
+    serverName = "";
+    withHost = true;
+    serverProtocol = "";
+    version = -1;
+    aiCount = 0;
+    tickRate = 10;
+    uniqueSessionId = '';
+    gameType = -1;
+    mapRaw = "";
+    mapName = "";
+    initMoney = 0;
+    timeLimit = 0;
+    scoreLimit = 0;
+    victoryCondition = 0;
+    incomeRate = 0;
     players: RawPlayer[] = [];
-    ingamePlayerId = 0
+    ingamePlayerId = 0;
     //TimeLeft
     //GameState
     //NeedPassword
@@ -154,15 +232,16 @@ export class RawGameData {
 }
 
 export class RawPlayer {
-    id = 0
-    aiLevel = -1
-    isObserver = false
-    alliance = 0
-    elo = 0
-    level = -1
-    name = "err"
-    deck?: DeckData
-    scoreLimit = 0
-    incomeRate = 0
-    mapPos = "player_null"
+    id = 0;
+    aiLevel = -1;
+    isObserver = false;
+    alliance = 0;
+    elo = 0;
+    level = -1;
+    name = "err";
+    deck?: DeckData;
+    scoreLimit = 0;
+    incomeRate = 0;
+    mapPos = "player_null";
+    winner: boolean = false;
 }
